1393406977b3c56680705d75d1c922a0e6d9a3ca
krevskiigrigorii@gmail.com <krevskiigrigorii@gmail.com>
2022-05-28 23:14:58 +0300

    fix example for 5.6

--- krev/src/data_structures/chapter5_bit_manipulation/Problem5_6.java
+++ krev/src/data_structures/chapter5_bit_manipulation/Problem5_6.java
@@ -5,7 +5,7 @@ package data_structures.chapter5_bit_manipulation;
  * 5.6 Conversion:
  * Write a function to determine the number of bits you would need to flip to convert integer A to integer B.
  * EXAMPLE
- * Input: 29 (or: 111131), 15 (or: 131111)
+ * Input: 29 (or: 11101), 15 (or: 01111)
  * Output: 2
  * Hints: #336, #369
  * <p>
