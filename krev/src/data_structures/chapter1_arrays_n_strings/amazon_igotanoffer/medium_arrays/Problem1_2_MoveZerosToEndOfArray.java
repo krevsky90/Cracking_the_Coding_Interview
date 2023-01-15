d10c3fa96938d3b3d4eea61ed5a72b06e36bd02a
krevskiigrigorii@gmail.com <krevskiigrigorii@gmail.com>
2023-01-15 13:24:20 +0300

    add official solution to ARRAY medium problem 2.1 from igotanoffer

--- krev/src/data_structures/chapter1_arrays_n_strings/amazon_igotanoffer/medium_arrays/Problem1_2_MoveZerosToEndOfArray.java
+++ krev/src/data_structures/chapter1_arrays_n_strings/amazon_igotanoffer/medium_arrays/Problem1_2_MoveZerosToEndOfArray.java
@@ -3,30 +3,29 @@ package data_structures.chapter1_arrays_n_strings.amazon_igotanoffer.medium_arra
 /**
  * https://igotanoffer.com/blogs/tech/array-interview-questions
  * https://leetcode.com/problems/move-zeroes/description/
- *
+ * <p>
  * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
- *
+ * <p>
  * Note that you must do this in-place without making a copy of the array.
- *
+ * <p>
  * Example 1:
  * Input: nums = [0,1,0,3,12]
  * Output: [1,3,12,0,0]
- *
+ * <p>
  * Example 2:
  * Input: nums = [0]
  * Output: [0]
- *
+ * <p>
  * Constraints:
  * 1 <= nums.length <= 104
  * -231 <= nums[i] <= 231 - 1
- *
+ * <p>
  * Follow up: Could you minimize the total number of operations done?
- *
  */
 public class Problem1_2_MoveZerosToEndOfArray {
     //time of solution ~ 20 mins
     //beats 99.9%
-    public void moveZeroes(int[] nums) {
+    public void moveZeroesKREV(int[] nums) {
         int amountOfNonZeroNums = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
@@ -35,6 +34,27 @@ public class Problem1_2_MoveZerosToEndOfArray {
                 nums[i] = temp;
                 amountOfNonZeroNums++;
             }
+        }
+    }
+
+    //Official
+    //beats 87.91%
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        // If the current element is not 0, then we need to
+        // append it just in front of last non 0 element we found.
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt] = nums[i];
+                lastNonZeroFoundAt++;
+            }
+        }
+
+        // After we have finished processing new elements,
+        // all the non-zero elements are already at beginning of array.
+        // We just need to fill remaining array with 0's.
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
         }
     }
 }
