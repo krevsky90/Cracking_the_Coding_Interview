70258ae5b2b3beb22ade1c811b23fbf4fe6b619b
krevskiigrigorii@gmail.com <krevskiigrigorii@gmail.com>
2023-02-26 19:54:00 +0300

    add solution of STRING medium problem 2.4 from igotanoffer

---
+++ krev/src/data_structures/chapter1_arrays_n_strings/amazon_igotanoffer/medium_strings/Problem2_4_LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,78 @@
+package data_structures.chapter1_arrays_n_strings.amazon_igotanoffer.medium_strings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * https://igotanoffer.com/blogs/tech/string-interview-questions
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ * <p>
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+ * <p>
+ * A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ * <p>
+ * Example 1:
+ * Input: digits = "23"
+ * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * <p>
+ * Example 2:
+ * Input: digits = ""
+ * Output: []
+ * <p>
+ * Example 3:
+ * Input: digits = "2"
+ * Output: ["a","b","c"]
+ * <p>
+ * Constraints:
+ * 0 <= digits.length <= 4
+ * digits[i] is a digit in the range ['2', '9'].
+ */
+public class Problem2_4_LetterCombinationsOfPhoneNumber {
+    /**
+     * KREVSKY SOLUTION
+     * idea ~ recursion
+     * time to solve ~ 20 mins of thinking + 18 with debugging
+     * 3 attempts (typos)
+     */
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.isEmpty()) return result;
+
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+
+        String prefix = "";
+        int pos = 0;
+        letterCombinations(map, result, prefix, pos, digits);
+
+        return result;
+    }
+
+    //digits = 23
+    //digits.size() = 2
+    //prefix = ""
+    //curNum = '2'
+    //letters = "abc"
+    //pos = 0
+    //curL = 'a'
+    public void letterCombinations(Map<Character, String> map, List<String> result, String prefix, int pos, String digits) {
+        if (pos >= digits.length()) {
+            result.add(prefix);
+            return;
+        }
+        Character curNum = digits.charAt(pos);
+        char[] letters = map.get(curNum).toCharArray();
+        for (char curL : letters) {
+            letterCombinations(map, result, prefix + curL, pos + 1, digits);
+        }
+    }
+}
