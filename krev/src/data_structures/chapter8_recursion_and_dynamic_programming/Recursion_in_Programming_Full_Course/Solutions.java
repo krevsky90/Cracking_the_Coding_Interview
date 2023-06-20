46bd872c8801c476fd39a0b632b4f70f876debb6
Grigory Krevsky <krevsky@netcracker.com>
2023-06-20 16:18:34 +0300

    add Recursion_in_Programming_Full_Course

---
+++ krev/src/data_structures/chapter8_recursion_and_dynamic_programming/Recursion_in_Programming_Full_Course/Solutions.java
@@ -0,0 +1,75 @@
+package data_structures.chapter8_recursion_and_dynamic_programming.Recursion_in_Programming_Full_Course;
+
+public class Solutions {
+    public static void main(String[] args) {
+        String input = "kayak";
+        System.out.println(isPalindrome(input));
+        System.out.println(findBinary(4, ""));
+    }
+
+
+    public static boolean isPalindrome(String input) {
+        if (input == null) return false;
+        if (input.length() <= 1) return true;
+
+        return input.charAt(0) == input.charAt(input.length() - 1) && isPalindrome(input.substring(1, input.length() - 1));
+    }
+
+    public static String findBinary(int decimal, String result) {
+        //base-case
+        if (decimal == 0) return result;
+
+        result = decimal % 2 + result;
+        return findBinary(decimal / 2, result);
+    }
+
+    public static void mergeSort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int start, int end) {
+        if (end <= start) return;
+
+        int mid = (end + start) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+
+    public static void merge(int[] arr, int start, int mid, int end) {
+        int i = start;
+        int j = mid + 1;
+        int[] tempArr = new int[end - start + 1];
+        int k = 0;  //index for tempArr
+        while (i < mid + 1 && j <= end) {
+            if (arr[i] <= arr[j]) {
+                tempArr[k] = arr[i];
+                i++;
+                k++;
+            } else {
+                tempArr[k] = arr[j];
+                j++;
+                k++;
+            }
+        }
+
+        //copy the rest part of left sub-array if it is not empty
+        while (i < mid + 1) {
+            tempArr[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        //copy the rest part of right sub-array if it is not empty
+        while (j < end) {
+            tempArr[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        //copy values from tempArr to arr
+        for (int c = start; c < end; c++) {
+            arr[c] = tempArr[c - start];
+        }
+    }
+}
