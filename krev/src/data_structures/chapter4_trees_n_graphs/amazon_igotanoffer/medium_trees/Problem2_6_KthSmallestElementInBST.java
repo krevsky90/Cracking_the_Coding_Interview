38cdb416ff067bf53d51ad62203d9263ff7d4944
Grigory Krevsky <krevsky@netcracker.com>
2023-06-20 16:10:37 +0300

    add two solutions of Problem2_6_KthSmallestElementInBST

--- krev/src/data_structures/chapter4_trees_n_graphs/amazon_igotanoffer/medium_trees/Problem2_6_KthSmallestElementInBST.java
+++ krev/src/data_structures/chapter4_trees_n_graphs/amazon_igotanoffer/medium_trees/Problem2_6_KthSmallestElementInBST.java
@@ -12,13 +12,60 @@ import java.util.*;
  */
 
 public class Problem2_6_KthSmallestElementInBST {
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        n3.left = n1;
+        n3.right = n4;
+        n1.right = n2;
+
+        int result = getKthSmallestElementOfBST(n3, 3);
+        System.out.println(result);
+    }
+
     /**
+     * Approach 0 - THE MOST OPTIMIZED
+     * Iterative Inorder Traversal
+     * info: https://www.youtube.com/watch?v=5LUXSvjmGCw
+     *
+     * This way one could speed up the solution because there is no need to build the entire inorder traversal, and one could stop after the kth element.
+     * Time complexity: O(H+k), where H is a tree height
+     * Space complexity: O(H), where average H = logN, worst H = N
+     */
+    public static int getKthSmallestElementOfBST(TreeNode root, int k) {
+        if (root == null) return Integer.MIN_VALUE;	//corner case or error
+
+        int counter = 0;
+        Stack<TreeNode> stack = new Stack<>();  //or LinkedList
+        TreeNode current = root;
+        while (!stack.isEmpty() || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            //we found the leftmost element of branch of the root
+            //let's remove element from the stack and increment counter (of visited nodes)
+            current = stack.pop();
+            counter++;
+            if (counter == k) return current.val;
+
+            //then go to right node of current! (rather than root! that was my mistake)
+            current = current.right;
+        }
+
+        return Integer.MIN_VALUE;	//means we didn't find the result
+    }
+
+    /**
+     * Approach 1:
      * KREVSKY SOLUTION:
      * idea - use priority queue
      * time to solve = 14 mins, 1 attempt
      * time complexity ~ (N*logN), because each insertion to priorityQueue takes O(logN). total amount of insertions is N
      */
-    public int kthSmallestKREVPriorityQueue(TreeNode root, int k) {
+    public static int kthSmallestKREVPriorityQueue(TreeNode root, int k) {
         PriorityQueue<Integer> q = new PriorityQueue<>();
 
         inOrderTraversal(root, q);
@@ -30,7 +77,7 @@ public class Problem2_6_KthSmallestElementInBST {
         return result.intValue();
     }
 
-    private void inOrderTraversal(TreeNode root, PriorityQueue<Integer> q) {
+    private static void inOrderTraversal(TreeNode root, PriorityQueue<Integer> q) {
         if (root != null) {
             inOrderTraversal(root.left, q);
             q.offer(root.val);
@@ -39,6 +86,7 @@ public class Problem2_6_KthSmallestElementInBST {
     }
 
     /**
+     * Approach 2.1:
      * KREVSKY SOLUTION
      * idea - use stack to store ALL the elements of the tree (first of all we need to pop the largest elements).
      * This should take O(n) time
@@ -48,7 +96,7 @@ public class Problem2_6_KthSmallestElementInBST {
      * <p>
      * NOTE: in my case it is sufficient to use simple ArrayList! see kthSmallestUsingArrayList method
      */
-    public int kthSmallestKREVStack(TreeNode root, int k) {
+    public static int kthSmallestKREVStack(TreeNode root, int k) {
         Stack<Integer> stack = new Stack<>();
 
         fillStack(root, stack);
@@ -61,7 +109,7 @@ public class Problem2_6_KthSmallestElementInBST {
     }
 
     //takes time ~ O(n)
-    private void fillStack(TreeNode root, Stack<Integer> stack) {
+    private static void fillStack(TreeNode root, Stack<Integer> stack) {
         if (root != null) {
             fillStack(root.right, stack);
             stack.push(root.val);
@@ -70,41 +118,47 @@ public class Problem2_6_KthSmallestElementInBST {
     }
 
     /**
-     * Approach 1: Recursive Inorder Traversal
+     * Approach 2.2
+     * KREVSKY SOLUTION
+     * idea - the same as initial (to fill array by inOrder traversal), BUT optimized - once we get k-th value - return it
+     * time ~ O(k)
+     * space ~ O(k)
+     */
+    public static int kthSmallestListStorageOptimized(TreeNode root, int k) {
+        return preOrder2(root, new ArrayList<>(), k);
+    }
+
+    private static int preOrder2(TreeNode root, List<Integer> list, int k) {
+        if (root == null) return Integer.MIN_VALUE;
+        int left = preOrder2(root.left, list, k);
+        if (left != Integer.MIN_VALUE) return left;
+
+        list.add(root.val);
+        if (list.size() == k) return root.val;
+
+        int right = preOrder2(root.right, list, k);
+        if (right != Integer.MIN_VALUE) return right;
+
+        return Integer.MIN_VALUE;	//instead of nothing
+    }
+
+    /**
+     * Approach 3:
+     * Recursive Inorder Traversal
      * https://leetcode.com/problems/kth-smallest-element-in-a-bst/solutions/284145/kth-smallest-element-in-a-bst/
      * Time complexity : O(N) to build a traversal.
      * Space complexity : O(N)
      */
-    public int kthSmallestUsingArrayList(TreeNode root, int k) {
+    public static int kthSmallestUsingArrayList(TreeNode root, int k) {
         List<Integer> nums = inorder(root, new ArrayList<>());
         return nums.get(k - 1);
     }
 
-    private List<Integer> inorder(TreeNode root, List<Integer> arr) {
+    private static List<Integer> inorder(TreeNode root, List<Integer> arr) {
         if (root == null) return arr;
         inorder(root.left, arr);
         arr.add(root.val);
         inorder(root.right, arr);
         return arr;
-    }
-
-    /**
-     * Approach 2: Iterative Inorder Traversal
-     * This way one could speed up the solution because there is no need to build the entire inorder traversal, and one could stop after the kth element.
-     * Time complexity: O(H+k), where H is a tree height
-     * Space complexity: O(H), where average H = logN, worst H = N
-     */
-    public int kthSmallest(TreeNode root, int k) {
-        LinkedList<TreeNode> stack = new LinkedList<>();
-
-        while (true) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            if (--k == 0) return root.val;
-            root = root.right;
-        }
     }
 }
