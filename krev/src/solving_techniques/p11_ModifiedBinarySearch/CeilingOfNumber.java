696579a66ef3d36d800af629f1a1daa718c98666
krevsky90 <krevskiigrigorii@gmail.com>
2023-11-20 18:35:45 +0300

    Modified Binary Search: add Ceiling of a Number from https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/639f1a1e44223ca42ca4a628

---
+++ krev/src/solving_techniques/p11_ModifiedBinarySearch/CeilingOfNumber.java
@@ -0,0 +1,58 @@
+package solving_techniques.p11_ModifiedBinarySearch;
+
+/**
+ * https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/639f1fd54e4f288d4a83ab44
+ *
+ * Given an array of numbers sorted in an ascending order, find the ceiling of a given number ?key?.
+ * The ceiling of the ?key? will be the smallest element in the given array greater than or equal to the ?key?.
+ *
+ * Write a function to return the index of the ceiling of the ?key?. If there isn?t any ceiling return -1.
+ *
+ * Example 1:
+ * Input: [4, 6, 10], key = 6
+ * Output: 1
+ * Explanation: The smallest number greater than or equal to '6' is '6' having index '1'.
+ *
+ * Example 2:
+ * Input: [1, 3, 8, 10, 15], key = 12
+ * Output: 4
+ */
+public class CeilingOfNumber {
+    public static void main(String[] args) {
+        int[] arr1 = {4, 6, 10};
+        int key1 = 6;
+        System.out.println(findCeiling(arr1, key1));
+
+        int[] arr2 = {1, 3, 8, 10, 15};
+        int key2 = 12;
+        System.out.println(findCeiling(arr2, key2));
+
+        int[] arr3 = {1, 3};
+        int key3 = 12;
+        System.out.println(findCeiling(arr3, key3));
+    }
+
+    public static int findCeiling(int[] arr, int key) {
+        int l = 0;
+        int h = arr.length - 1;
+        while (l <= h) {
+            int mid = (l + h)/2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] > key) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        if (arr[l] < key) {
+            System.out.println("Ceiling does not exist!");
+            return -100500; //error
+        } else {
+            return l;
+        }
+
+
+    }
+}
---
+++ krev/src/solving_techniques/p11_ModifiedBinarySearch/readme.txt
@@ -0,0 +1,17 @@
+https://interviewnoodle.com/grokking-leetcode-a-smarter-way-to-prepare-for-coding-interviews-e86d5c9fe4e1
+https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/639f1a1e44223ca42ca4a628
+
+Whenever we are given a sorted Array or LinkedList or Matrix,
+and we are asked to find a certain element, the best algorithm we can use is the Binary Search.
+
+Sequence of problems:
+1) Order-agnostic Binary Search (easy) - todo
+2) Ceiling of a Number (medium) - done
+3) Next Letter (medium) - todo
+4) Number Range (medium) - todo
+5) Search in a Sorted Infinite Array (medium) - todo
+6) Minimum Difference Element (medium) - todo
+7) Bitonic Array Maximum (easy) - todo
+8) Problem Challenge 1: Search Bitonic Array (medium) - todo
+9) Problem Challenge 2: Search in Rotated Array (medium) - todo
+10) Problem Challenge 3: Rotation Count (medium) - todo
