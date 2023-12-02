04b2d8f8b636deb15631e8a867d5d625a1611f26
krevsky90 <krevskiigrigorii@gmail.com>
2023-12-02 19:23:28 +0300

    In-place Reversal of a LinkedList: add Reverse every K-element Sub-list from https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/6394dfe0a05eca17327534ff

---
+++ krev/src/solving_techniques/p6_InPlaceReversalLinkedList/ReverseEveryKElementSublist.java
@@ -0,0 +1,140 @@
+package solving_techniques.p6_InPlaceReversalLinkedList;
+
+import data_structures.chapter2_linked_lists.LinkedListNode;
+
+/**
+ * https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/6394dfe0a05eca17327534ff
+ * OR (+-)
+ * 25. Reverse Nodes in k-Group
+ * https://leetcode.com/problems/reverse-nodes-in-k-group/
+ * <p>
+ * Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+ * k is a positive integer and is less than or equal to the length of the linked list.
+ * If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+ * You may not alter the values in the list's nodes, only nodes themselves may be changed.
+ * <p>
+ * Example 1:
+ * Input: head = [1,2,3,4,5], k = 2
+ * Output: [2,1,4,3,5]
+ * <p>
+ * Example 2:
+ * Input: head = [1,2,3,4,5], k = 3
+ * Output: [3,2,1,4,5]
+ * <p>
+ * Constraints:
+ * The number of nodes in the list is n.
+ * 1 <= k <= n <= 5000
+ * 0 <= Node.val <= 1000
+ * <p>
+ * Follow-up: Can you solve the problem in O(1) extra memory space?
+ * <p>
+ * NOTE: I will solve leetcode's variation
+ */
+public class ReverseEveryKElementSublist {
+    /**
+     * KREVSKY SOLUTION:
+     * todo
+     * time to solve ~ 60 mins
+     * time ~ O(n)
+     * space ~ O(1)
+     * 1 attempt
+     */
+
+
+    // it=0
+    // k=2
+
+    // 2      ->        1      ->        4    ->       3       ->       6      ->       5     ->   null
+    // head
+    //                                                                curHead
+    //                                                                                  curEnd                    
+    //                                                                                             curNode
+    //                                                                                             nextHead
+
+
+    public LinkedListNode reverseKGroup(LinkedListNode head, int k) {
+        int it = 0;
+        LinkedListNode curHead = head;
+        LinkedListNode curEnd = null;
+        LinkedListNode nextHead = null;
+        LinkedListNode curNode = head;
+
+        while (curNode != null && it < k) {
+            curNode = curNode.next;
+            it++;
+        }
+
+        if (it == k) {
+            nextHead = curNode;
+            //reverse list with curHead
+            curHead = reverseList(curHead, k);
+            //move through sublist to find curEnd
+            curEnd = findEndNode(curHead);
+
+            it = 0;
+        } else {
+            //it means while-loop stopped because the whole linked list is shorter than K => nothing to reverse
+            return head;
+        }
+
+        head = curHead;
+
+        while (curNode != null) {
+            while (curNode != null && it < k) {
+                curNode = curNode.next;
+                it++;
+            }
+
+            if (it == k) {
+                curHead = nextHead;
+                nextHead = curNode;
+                //reverse list with curHead
+                curHead = reverseList(curHead, k);
+
+                //link curEnd from previous sublist and curHead - head of current reversed sublist
+                curEnd.next = curHead;
+
+                //move through sublist to find curEnd
+                curEnd = findEndNode(curHead);
+
+                it = 0;
+            } else {
+                //it means while stops because the rest part of linked list is shorter than K => nothing to reverse
+                curEnd.next = nextHead;
+                // break; - not necessary since curNode = null => while-loop will be stopped
+            }
+        }
+
+        return head;
+    }
+
+    // null <- 1 <- 2   3, k = 2
+    // prev = 2
+    // cur = 3
+    // i = 1
+    //     next = 3
+
+    private LinkedListNode reverseList(LinkedListNode head, int k) {
+        LinkedListNode prevNode = null;
+        LinkedListNode curNode = head;
+        for (int i = 0; i < k; i++) {
+            LinkedListNode nextNode = curNode.next;
+            curNode.next = prevNode;
+            prevNode = curNode;
+            curNode = nextNode;
+        }
+
+        return prevNode;
+    }
+
+    private LinkedListNode findEndNode(LinkedListNode head) {
+        LinkedListNode end = head;
+        LinkedListNode cur = head;
+        while (cur != null) {
+            end = cur;
+            cur = cur.next;
+        }
+
+        return end;
+    }
+}
--- krev/src/solving_techniques/p6_InPlaceReversalLinkedList/readme.txt
+++ krev/src/solving_techniques/p6_InPlaceReversalLinkedList/readme.txt
@@ -24,6 +24,6 @@ return prev
 Sequence of problems:
 1) Reverse a LinkedList (easy) - done
 2) Reverse a Sub-list (medium) - done
-3) Reverse every K-element Sub-list (medium) - todo
+3) Reverse every K-element Sub-list (medium) = https://leetcode.com/problems/reverse-nodes-in-k-group/ (hard) - done
 4) Problem Challenge 1: Reverse alternating K-element Sub-list (medium) - todo
 5) Problem Challenge 2: Rotate a LinkedList (medium) - todo
