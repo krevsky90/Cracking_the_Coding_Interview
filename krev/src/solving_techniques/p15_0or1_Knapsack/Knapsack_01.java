d0bf8beb206621b1b4959c5ce7b464c486f6228c
krevsky90 <krevskiigrigorii@gmail.com>
2023-12-08 18:15:56 +0300

    Knapsack: add Knapsack_01 for https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/63a388c3b7eef54e90110f88

---
+++ krev/src/solving_techniques/p15_0or1_Knapsack/Knapsack_01.java
@@ -0,0 +1,84 @@
+package solving_techniques.p15_0or1_Knapsack;
+
+/**
+ * https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/63a388c3b7eef54e90110f88
+ *
+ * Given the weights and profits of ?N? items, we are asked to put these items in a knapsack with a capacity ?C.?
+ * The goal is to get the maximum profit out of the knapsack items.
+ * Each item can only be selected once, as we don?t have multiple quantities of any item.
+ * Let?s take Merry?s example, who wants to carry some fruits in the knapsack to get maximum profit. Here are the weights and profits of the fruits:
+ * Items: { Apple, Orange, Banana, Melon }
+ * Weights: { 2, 3, 1, 4 }
+ * Profits: { 4, 5, 3, 7 }
+ * Knapsack capacity: 5
+ */
+public class Knapsack_01 {
+    public static void main(String[] args) {
+        String[] items = {"Apple", "Orange", "Banana", "Melon"};
+        int[] weights = {2, 3, 1, 4};
+        int[] profits = {4, 5, 3, 7};
+        int capacity = 5;
+
+        int result = maxProfitOptimizedNP(items, weights, profits, capacity);
+        System.out.println("max Profit = " + result);
+    }
+
+    /**
+     * SOLUTION #1:
+     * time complexity ~ O(2^items.length)
+     * space complexity ~ O(1) or O(items.length)
+     */
+    public static int maxProfitNaiveBinaryVector(String[] items, int[] weights, int[] profits, int capacity) {
+        int amountOfCombinations = 1 << items.length;
+        int maxProfit = 0;
+        for (int i = 0; i < amountOfCombinations; i++) {
+            maxProfit = Math.max(maxProfit, maxProfitNaiveBinaryVectorHelper(items, weights, profits, capacity, i));
+        }
+
+        return maxProfit;
+    }
+
+    public static int maxProfitNaiveBinaryVectorHelper(String[] items, int[] weights, int[] profits, int capacity, int combination) {
+        int totalProfit = 0;
+        int totalWeight = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (((combination >> i) & 1) == 1) {
+                totalWeight += weights[i];
+                if (totalWeight > capacity) {
+                    //this combination can not exist
+                    return 0;
+                } else {
+                    totalProfit += profits[i];
+                }
+            }
+        }
+
+        return totalProfit;
+    }
+
+    /**
+     * SOLUTION #2:
+     * time complexity ~ O(items.length*capacity)
+     * space complexity ~ O(items.length*capacity)
+     */
+    public static int maxProfitOptimizedNP(String[] items, int[] weights, int[] profits, int capacity) {
+        int[][] dp = new int[items.length][capacity + 1];
+
+        for (int i = 0; i < items.length; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else {
+                    if (weights[i] > w) {
+                        dp[i][w] = dp[i-1][w];
+                    } else {
+                        dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w - weights[i]] + profits[i]);
+                    }
+                }
+            }
+        }
+
+        return dp[items.length - 1][capacity];
+    }
+
+}
---
+++ krev/src/solving_techniques/p15_0or1_Knapsack/readme.txt
@@ -0,0 +1,42 @@
+https://interviewnoodle.com/grokking-leetcode-a-smarter-way-to-prepare-for-coding-interviews-e86d5c9fe4e1
+https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/63a38826eed2aca55cd622d9
+
+Theory:
+https://leetcode.com/discuss/study-guide/1152328/01-Knapsack-Problem-and-Dynamic-Programming - todo: read it!
+or
+https://www.youtube.com/watch?v=xCbYmUPvc2Q
+
+NOTE: 0/1 means we cannot split the elements: we include some particular element or NOT
+
+Problem:
+there are some items. For each item:
+i-th item i has weight (Wi) and value (Vi)
+It is necessary to find the combination of the elements such as
+1) total weight <= W
+2) total value is maximum
+
+Dynamic programming:
+IF
+    i - item, the row we are in
+    W - maxWeight, the column we are in
+THEN
+    V[i,W] - looks like table
+
+How to fill the table:
+    1) IF i = 0 OR W = 0 THEN V(i,W)
+
+    2) IF Wi <= W THEN V[i,W] = V[i-1,W]
+       ELSE V[i,W] = max(V[i-1][W], V[i-1][W-Wi] + Vi
+
+Sequence of problems:
+1) 0/1 Knapsack (medium) - done
+2) Equal Subset Sum Partition (medium) - todo
+3) Subset Sum (medium) - todo
+4) Minimum Subset Sum Difference (hard) - todo
+5) Problem Challenge 1: Count of Subset Sum (hard) - todo
+6) Problem Challenge 2: Target Sum (hard) - todo
+
+
+
+
+
