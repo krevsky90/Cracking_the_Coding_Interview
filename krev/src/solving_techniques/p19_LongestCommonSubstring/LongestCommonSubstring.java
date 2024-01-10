cd834f10f103f6716069199057ee99659aacff01
krevsky90 <krevskiigrigorii@gmail.com>
2024-01-10 14:37:08 +0300

    Longest Common Substring: add Longest Common Substring from https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/6394ef9cc6ebad0cc48b8076

---
+++ krev/src/solving_techniques/p19_LongestCommonSubstring/LongestCommonSubstring.java
@@ -0,0 +1,119 @@
+package solving_techniques.p19_LongestCommonSubstring;
+
+/**
+ * https://www.designgurus.io/course-play/grokking-dynamic-programming/doc/637f5929a842de869cd616c5
+ * OR
+ * (similar, but for integers)
+ * 718. Maximum Length of Repeated Subarray
+ * https://leetcode.com/problems/maximum-length-of-repeated-subarray/description/
+ *
+ */
+public class LongestCommonSubstring {
+    public static void main(String[] args) {
+        String s11 = "abdca";
+        String s12 = "cbda";
+        System.out.println(longestCommonSubstring(s11, s12)); //expect "bd"
+
+        String s21 = "passport";
+        String s22 = "ppsspt";
+        System.out.println(longestCommonSubstring(s21, s22)); //expect "ssp"
+    }
+    /**
+     * from grokking-dynamic-programming
+     * Given two strings 's1' and 's2', find the length of the longest substring which is common in both the strings.
+     *
+     * Example 1:
+     * Input: s1 = "abdca"
+     *        s2 = "cbda"
+     * Output: 2
+     * Explanation: The longest common substring is "bd".
+     *
+     * Example 2:
+     * Input: s1 = "passport"
+     *        s2 = "ppsspt"
+     * Output: 3
+     * Explanation: The longest common substring is "ssp".
+     */
+
+    /**
+     * implementation the idea f https://www.youtube.com/watch?v=UZRkpGk943Q
+     * (described in src/solving_techniques/p19_LongestCommonSubstring/readme.txt)
+     */
+
+    /**
+     *       c b d a (j)
+     *   | 0 0 0 0 0
+     * a | 0 0 0 0 1
+     * b | 0 0 1 0 0
+     * d | 0 0 0 2 0
+     * c | 0 1 0 0 0
+     * a | 0 0 0 0 1
+     *(i)
+     */
+    public static String longestCommonSubstring(String s1, String s2) {
+        if (s1 == null || s2 == null) return null;
+        if (s1.isEmpty() || s2.isEmpty()) return "";
+
+        String out = "";
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        //since 0th row and column should be filled by 0 (it means that empty string cannot have non-empty substring),
+        // just start both for-loops with 1
+        for (int i = 1; i < s1.length() + 1; i++) {
+            for (int j = 1; j < s2.length() + 1; j++) {
+                //use i-1 and j-1 to avoid IndexOutOfBoundException (since max i = s1.length(), for example)
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    //DO NOT UNDERSTAND CLEARLY!
+                    // but smth like this: it means that the elements [xxx, i] will not be common substring (listen here https://youtu.be/UZRkpGk943Q?t=580)
+                    dp[i][j] = 0;
+                }
+
+                //update longest common substring
+                if (out.length() < dp[i][j]) {
+                    //for example, get substring from s1 string:
+                    //BE CAREFUL WITH INDEXES!
+                    //and always draw the table like in the comment above
+                    out = s1.substring(i - dp[i][j], i);
+                }
+            }
+        }
+
+        return out;
+    }
+
+    /**
+     * https://leetcode.com/problems/maximum-length-of-repeated-subarray/description/
+     * Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.
+     *
+     * Constraints:
+     * 1 <= nums1.length, nums2.length <= 1000
+     * 0 <= nums1[i], nums2[i] <= 100
+     *
+     * idea is the same
+     * time to solve ~ 3 mins
+     * time ~ O(nums1.length * nums2.length)
+     * space ~ O(nums1.length * nums2.length)
+     *
+     * 1 attempt
+     */
+    public int findLength(int[] nums1, int[] nums2) {
+        int result = 0;
+
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+
+        for (int i = 1; i < nums1.length + 1; i++) {
+            for (int j = 1; j < nums2.length + 1; j++) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 0;
+                }
+
+                result = Math.max(result, dp[i][j]);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ krev/src/solving_techniques/p19_LongestCommonSubstring/readme.txt
@@ -0,0 +1,26 @@
+https://interviewnoodle.com/grokking-leetcode-a-smarter-way-to-prepare-for-coding-interviews-e86d5c9fe4e1
+https://www.designgurus.io/course-play/grokking-dynamic-programming/doc/637f5929a842de869cd616c5
+
+Theory:
+info: https://www.youtube.com/watch?v=UZRkpGk943Q
+1) i - iterator for string s1, j - iterator for string s2
+2) create int[][] dp = new int[s1.len + 1][s2.len + 1] and fill 0-th row and column by 0
+3) fill dp table: IF s1.charAt(i-1) == s2.charAt(j-1) THEN dp[i][j] = dp[i-1][j-1] + 1
+4) maxLength = max(maxLength, dp[i][j])
+NOTE: the space can be optimized since i-th row depends only on i-1-th
+
+
+Sequence of problems:
+1) Longest Common Substring - in progress
+2) Longest Common Subsequence - todo
+3) Minimum Deletions & Insertions to Transform a String into another - todo
+4) Longest Increasing Subsequence - todo
+5) Maximum Sum Increasing Subsequence - todo
+6) Shortest Common Super-sequence - todo
+7) Minimum Deletions to Make a Sequence Sorted - todo
+8) Longest Repeating Subsequence - todo
+9) Subsequence Pattern Matching - todo
+10) Longest Bitonic Subsequence - todo
+11) Longest Alternating Subsequence - todo
+12) Edit Distance - todo
+13) Strings Interleaving - todo
