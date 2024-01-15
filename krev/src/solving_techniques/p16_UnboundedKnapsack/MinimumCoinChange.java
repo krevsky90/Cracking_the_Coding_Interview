e7076410384476a70c3c30533c5d96915260ff85
krevsky90 <krevskiigrigorii@gmail.com>
2024-01-15 19:48:43 +0300

    Unbounded Knapsack: add solutions to MinimumCoinChange

--- krev/src/solving_techniques/p16_UnboundedKnapsack/MinimumCoinChange.java
+++ krev/src/solving_techniques/p16_UnboundedKnapsack/MinimumCoinChange.java
@@ -37,7 +37,9 @@ public class MinimumCoinChange {
     public static void main(String[] args) {
         int[] coins1 = {1, 2, 5};
         int amount1 = 11;
-        System.out.println(coinChange(coins1, amount1));   //3
+        System.out.println("coinChange = " + coinChange(coins1, amount1));   //3
+        System.out.println("coinCount = " + coinCount(coins1, amount1));    //3
+        System.out.println("coinCountDP = " + coinCountDP(coins1, amount1));    //3
 
         int[] coins2 = {2};
         int amount2 = 3;
@@ -113,11 +115,13 @@ public class MinimumCoinChange {
      * time to solve ~ 50 mins
      * time ~ O(coins.length*amount)
      * space ~ O(amount)
+     * NOTE: it is also BAD idea to use MAX_VALUE! since we need to handle case 1 + MAX_VALUE (which = MIN_VALUE).
+     *       it is quite better to use, for example, 100000 instead of MAX_VALUE - see coinCountDP(..) method
      * ~5 attempts:
      * - it is bad idea to set MAX_VALUE as default value for memo array!!! because we should distinct 2 cases:
      * a) we have not tried to fill i-th cell (i.e. -1)
      * b) it is impossible to change the amount = i => we set MAX_VALUE. One we come to this cell again - we return MAX_VALUE immediately!
-     * NOTE: yes, finally we return -1 if we can't change the amount. But during the soluton "-1" means not touched cell/amount
+     * NOTE: yes, finally we return -1 if we can't change the amount. But during the solution "-1" means not touched cell/amount
      * - need to add condition "if (tempMin != Integer.MAX_VALUE)"
      * - need to add condition "if (subResult != -1)"
      */
@@ -162,4 +166,71 @@ public class MinimumCoinChange {
         memo[amount] = tempMin;
         return memo[amount];
     }
+
+    /**
+     * SOLUTION using APPROACH #1 - i.e. the logic "include or exclude" the element https://www.youtube.com/watch?v=ZI17bgz07EE
+     * idea:
+     * result = min(1 + count(coins, amount - coins[i], i), count(coins, amount, i + 1))
+     */
+    public static int coinCount(int[] coins, int amount) {
+        return coinCount(coins, amount, 0);
+    }
+
+    private static int coinCount(int[] coins, int amount, int startIdx) {
+        if (amount == 0) {
+            return 0;
+        }
+
+        //
+        if (startIdx == coins.length) {
+            return 100000;   //reflect that this path of denomination is impossible
+        }
+
+        if (coins[startIdx] > amount) {
+            //we can't use 'include' option => result = notTake option
+            return coinCount(coins, amount, startIdx + 1);
+        } else {
+            int takeCoin = 1 + coinCount(coins, amount - coins[startIdx], startIdx);
+            int notTakeCoin = coinCount(coins, amount, startIdx + 1);
+            return Math.min(takeCoin, notTakeCoin);
+        }
+    }
+
+    /**
+     * SOLUTION #3:
+     * https://youtu.be/ZI17bgz07EE?t=932
+     * the same idea as for SOLUTION #2, BUT using dp table
+     * dp = [coins.len + 1][amount + 1]
+     * dp[i][j] - min number of coins needed to form amount = j, using first i coins
+     *      to exclude: dp[i][j] = dp[i-1][j]
+     *      to include: dp[i][j] = 1 + dp[i][j - coins[i-1]] - NOTE! be careful with case when coins[i-1] > j! then we cannot include the element!
+     * dp[i][j] = min(exclude, include)
+     * bound cases:
+     *      all dp[i][0] = MAX_VALUE
+     *      dp[0][j] = 0
+     *
+     * time ~ O(coins.length*amount)
+     * space ~ O(coins.length*amount)
+     */
+    public static int coinCountDP(int[] coins, int amount) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+
+        for (int i = 0; i < coins.length + 1; i++) {
+            for (int j = 0; j < amount + 1; j++) {
+                if (i == 0) {
+                    dp[i][j] = Integer.MAX_VALUE;
+                } else if (j == 0) {
+                    dp[i][j] = 0;
+                } else if (coins[i-1] > j) {
+                    //we can not include i-1-th coin. we can only use 'exclude' option
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.min(1 + dp[i][j - coins[i-1]], dp[i-1][j]);
+                }
+            }
+        }
+
+        return dp[coins.length][amount] != Integer.MAX_VALUE ? dp[coins.length][amount] : -1;
+    }
+
 }
