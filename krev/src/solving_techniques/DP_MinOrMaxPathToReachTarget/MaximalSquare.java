a9afc422cd585cfae236cf2eef26adfb1ff41e78
krevsky90 <krevskiigrigorii@gmail.com>
2024-04-11 18:37:03 +0300

    add description to MaximalSquare

--- krev/src/solving_techniques/DP_MinOrMaxPathToReachTarget/MaximalSquare.java
+++ krev/src/solving_techniques/DP_MinOrMaxPathToReachTarget/MaximalSquare.java
@@ -2,6 +2,29 @@ package solving_techniques.DP_MinOrMaxPathToReachTarget;
 
 import java.util.Arrays;
 
+/**
+ * 221. Maximal Square
+ * https://leetcode.com/problems/maximal-square/
+ *
+ * Example 1:
+ * Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+ * Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * Output: 4
+ *
+ * Example 2:
+ * Input: matrix = [["0","1"],["1","0"]]
+ * Output: 1
+ *
+ * Example 3:
+ * Input: matrix = [["0"]]
+ * Output: 0
+ *
+ * Constraints:
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 300
+ * matrix[i][j] is '0' or '1'.
+ */
 public class MaximalSquare {
     public static void main(String[] args) {
         char[][] matrix = {
