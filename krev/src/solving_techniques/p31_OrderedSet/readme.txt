10b735aee89352ccc2feca176feb993d965aff91
krevsky90 <krevskiigrigorii@gmail.com>
2024-06-08 15:59:27 +0300

    Ordered Set: add MyCalendarI add OrderedSetExamples

--- krev/src/solving_techniques/p31_OrderedSet/readme.txt
+++ krev/src/solving_techniques/p31_OrderedSet/readme.txt
@@ -6,14 +6,16 @@ An Ordered Set is a powerful data structure that blends the uniqueness of a set 
 Unlike traditional sets, where the elements are unsorted, an ordered set maintains the sequence of its elements,
 making it a versatile choice for many applications.
 
-todo: find more theory
 As I understand, the idea is to user Ordered collections:
 TreeSet, TreeMap etc
+
+see examples here:
+src/solving_techniques/p31_OrderedSet/theory/OrderedSetExamples.java
 
 Sequence of problems:
 1) Merge Similar Items (easy) - https://leetcode.com/problems/merge-similar-items - done
 2) 132 Pattern (medium) - https://leetcode.com/problems/132-pattern - done
-3) My Calendar I (medium) - https://leetcode.com/problems/my-calendar-i - todo
+3) My Calendar I (medium) - https://leetcode.com/problems/my-calendar-i - done
 4) Longest Continuous Subarray (medium) - ?? https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit ?? - todo
 
 more problems are here:
---
+++ krev/src/solving_techniques/p31_OrderedSet/theory/OrderedSetExamples.java
@@ -0,0 +1,36 @@
+package solving_techniques.p31_OrderedSet.theory;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class OrderedSetExamples {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(4, "Four");
+        treeMap.put(1, "One");
+        treeMap.put(7, "Seven");
+        treeMap.put(3, "Three");
+        treeMap.put(9, "Nine");
+        //stores in acs order: 1(One) 3(Three) 4(Four) 7(Seven) 9(Nine)
+
+        //NOTE: each operation takes O(logN)
+        System.out.println(treeMap.lowerEntry(7));  //4
+        System.out.println(treeMap.lowerKey(7));    //4
+        System.out.println(treeMap.higherEntry(7)); //9
+        System.out.println(treeMap.higherKey(7));   //9
+        System.out.println(treeMap.ceilingKey(6));  //7 - closest key that is greater
+        System.out.println(treeMap.floorKey(6));  //4 - closest key that is less
+        System.out.println(treeMap.firstKey());  //1
+        System.out.println(treeMap.lastKey());  //9
+
+        treeMap.descendingKeySet(); // 9 7 4 3 1
+
+        treeMap.headMap(3); //1(One) - part of map: to the beginning to given key
+        treeMap.headMap(3, true); //1(One) 3(Three) - part of map: to the beginning to given key inclusively
+        treeMap.tailMap(4); //4(Four) 7(Seven) 9(Nine) - part of map: from given key to the end
+        treeMap.tailMap(4, false); // 7(Seven) 9(Nine) - part of map: from given key to the end, can exclude given key
+
+        TreeSet<Integer> ts = new TreeSet<>();
+        //NOTE: the same methods as for TreeMap
+    }
+}
