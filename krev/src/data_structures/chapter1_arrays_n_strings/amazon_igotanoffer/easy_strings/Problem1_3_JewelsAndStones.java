6eeacbb993ab5969cc4fc9cee9e97e0b0a1b5e5f
krevsky90 <krevskiigrigorii@gmail.com>
2024-08-22 21:38:50 +0300

    Set: change solution for Problem1_3_JewelsAndStones

--- krev/src/data_structures/chapter1_arrays_n_strings/amazon_igotanoffer/easy_strings/Problem1_3_JewelsAndStones.java
+++ krev/src/data_structures/chapter1_arrays_n_strings/amazon_igotanoffer/easy_strings/Problem1_3_JewelsAndStones.java
@@ -3,10 +3,16 @@ package data_structures.chapter1_arrays_n_strings.amazon_igotanoffer.easy_string
 import java.util.HashSet;
 
 /**
+ * 771. Jewels and Stones (easy)
+ * https://leetcode.com/problems/jewels-and-stones
+ * OR
  * https://igotanoffer.com/blogs/tech/string-interview-questions
- * https://leetcode.com/problems/jewels-and-stones/solutions/113553/c-java-python-set-solution-o-j-s/
  *
- * You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+ * #Company: Adobe Alibaba Amazon Apple Baidu Facebook Google Yahoo Yandex
+ *
+ * You're given strings jewels representing the types of stones that are jewels,
+ *      and stones representing the stones you have. Each character in stones is a type of stone you have.
+ * You want to know how many of the stones you have are also jewels.
  *
  * Letters are case sensitive, so "a" is considered a different type of stone from "A".
  *
@@ -25,27 +31,16 @@ import java.util.HashSet;
  */
 public class Problem1_3_JewelsAndStones {
     /**
-     * O(J*S)
-     */
-    public int numJewelsInStonesKREV(String jewels, String stones) {
-        char[] arr = jewels.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            stones = stones.replaceAll("" + arr[i], "1");
-        }
-
-        int result = 0;
-        for(int j = 0; j < stones.length(); j++) {
-            if (stones.charAt(j) == '1') {
-                result++;
-            }
-        }
-
-        return result;
-    }
-
-    /**
-     * O(J+S)
-     * The IDEA: search through HashSet is O(1) -> let's search letters from stones string in hashSet created from jewels
+     * KREVSKY SOLUTION
+     * time to solve ~ 8 mins
+     * idea:
+     * 1) traverse through jewels and form HashSet
+     * 2) traverse through stones and for each element check (using HashSet -> O(1)) if it is jewel or not
+     * time ~ O(J+S)
+     * space ~ O(1)
+     * 1 attempt
+     *
+     * BEATS ~ 58%
      */
     public int numJewelsInStones(String jewels, String stones) {
         HashSet setJ = new HashSet();
