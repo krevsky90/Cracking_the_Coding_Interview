6cc397de237d5458e99514c126ad0a9cfad28eb8
krevsky90 <krevskiigrigorii@gmail.com>
2024-09-27 20:11:18 +0300

    Modified Binary Search: add solution

--- krev/src/solving_techniques/p11_ModifiedBinarySearch/ProblemChallenge2_SearchInRotatedArray.java
+++ krev/src/solving_techniques/p11_ModifiedBinarySearch/ProblemChallenge2_SearchInRotatedArray.java
@@ -5,7 +5,7 @@ package solving_techniques.p11_ModifiedBinarySearch;
  * OR
  * 33. Search in Rotated Sorted Array
  * https://leetcode.com/problems/search-in-rotated-sorted-array/
- *
+ * <p>
  * There is an integer array nums sorted in ascending order (with distinct values).
  * Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length)
  * such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
@@ -13,19 +13,19 @@ package solving_techniques.p11_ModifiedBinarySearch;
  * Given the array nums after the possible rotation and an integer target,
  * return the index of target if it is in nums, or -1 if it is not in nums.
  * You must write an algorithm with O(log n) runtime complexity.
- *
+ * <p>
  * Example 1:
  * Input: nums = [4,5,6,7,0,1,2], target = 0
  * Output: 4
- *
+ * <p>
  * Example 2:
  * Input: nums = [4,5,6,7,0,1,2], target = 3
  * Output: -1
- *
+ * <p>
  * Example 3:
  * Input: nums = [1], target = 0
  * Output: -1
- *
+ * <p>
  * Constraints:
  * 1 <= nums.length <= 5000
  * -10000 <= nums[i] <= 10000
@@ -37,35 +37,76 @@ public class ProblemChallenge2_SearchInRotatedArray {
     public static void main(String[] args) {
         int[] nums1 = {3, 1};
         int target1 = 1;
-        System.out.println(search(nums1, target1));
+        System.out.println(search2(nums1, target1));
 
-        int[] nums2 = {4,5,6,7,0,1,2};
+        int[] nums2 = {4, 5, 6, 7, 0, 1, 2};
         int target2 = 0;
-        System.out.println(search(nums2, target2));
+        System.out.println(search2(nums2, target2));
 
         int[] nums3 = {1};
         int target3 = 0;
-        System.out.println(search(nums3, target3));
+        System.out.println(search2(nums3, target3));
     }
 
     /**
-     * KREVSKY SOLUTION:
+     * info:
+     * https://www.youtube.com/watch?v=WJesTo72lFY&list=PLUPSMCjQ-7od5IVz8ug6D-apxFLkDTsoy&index=77
+     * similar to KREVSKY SOLUTION, BUT:
+     * 1) remove part for usual binary search, since it is covered by our algorithm
+     * 2) simplified else-part doing it symmetric as if-part
+     *
+     * BEATS ~ 100%
+     */
+    public static int search1(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+
+            if (nums[low] <= nums[mid]) {
+                //it means that low - mid part is sorted in ascending order
+                //but mid - high - not sorted
+                if (nums[low] <= target && target < nums[mid]) {
+                    //it means that target belongs to sorted part
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                //it means that mid - high part is sorted in ascending order - see p.2
+                if (nums[mid] < target && target <= nums[high]) {
+                    //it means that target belongs to sorted part
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        //target is not found
+        return -1;
+    }
+
+    /**
+     * KREVSKY SOLUTION #2:
      * time to solve ~ 45 mins
      * time ~ O(logN)
      * space ~ O(1)
      * 4 attempts:
-     *  1) forget "=" in expression: nums[mid] >= nums[low])
-     *  2) forget "&& target >= nums[low]" in expression: if (nums[mid] > target && target >= nums[low])
-     *  3) set "int mid = (low + high)/2;"  out of while loop for case of NOT rotated array
+     * 1) forget "=" in expression: nums[mid] >= nums[low])
+     * 2) forget "&& target >= nums[low]" in expression: if (nums[mid] > target && target >= nums[low])
+     * 3) set "int mid = (low + high)/2;"  out of while loop for case of NOT rotated array
      */
-    public static int search(int[] nums, int target) {
+    public static int search2(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
 
         if (nums[low] < nums[high]) {
             //if the array is NOT rotated
             while (low <= high) {
-                int mid = (low + high)/2;
+                int mid = (low + high) / 2;
                 if (nums[mid] == target) return mid;
 
                 if (nums[mid] > target) {
@@ -77,7 +118,7 @@ public class ProblemChallenge2_SearchInRotatedArray {
         } else {
             //if the array is rotated => nums[low] > nums[high]
             while (low <= high) {
-                int mid = (low + high)/2;
+                int mid = (low + high) / 2;
                 if (nums[mid] == target) return mid;
 
                 if (nums[mid] >= nums[low]) {
