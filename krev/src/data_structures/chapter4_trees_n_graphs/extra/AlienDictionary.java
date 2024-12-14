748dbf8ef3d07d2c76e36a7ad5ec77b8d6812d86
krevsky90 <krevskiigrigorii@gmail.com>
2024-12-14 18:13:36 +0300

    Topological Sort: fix AlienDictionary #269

--- krev/src/data_structures/chapter4_trees_n_graphs/extra/AlienDictionary.java
+++ krev/src/data_structures/chapter4_trees_n_graphs/extra/AlienDictionary.java
@@ -29,12 +29,117 @@ import java.util.*;
  * words: ["z", "x"]
  * Output: "zx"
  */
-
-//
 public class AlienDictionary {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("wrt", "wrf", "er", "ett", "rftt");
         System.out.println(alienDictionary(words));
+    }
+
+    /**
+     * KREVSKY #2
+     * info: https://www.youtube.com/watch?v=0nI8_RbVeb4&list=PLUPSMCjQ-7odm9bh0iW6WOCfS6wiJETnt&index=18
+     * idea:
+     * 1) build list of letters
+     * 2) build adjMap: letter -> list of 'further' letters (since it is alphabet, it is expected that the list will contain 0 or 1 element if data is correct
+     * 3) use topological sort DFS function:
+     * - keep visited letters
+     * - keep cycle letters
+     * - keep sb to store the final letter, i.e. we processed its children
+     *
+     * edge cases:
+     * 1) if given words[] is not lexicographically sorted (i.e. incorrectness is like apple, app), then return ""
+     *
+     * N - words.length, S - max length of the words
+     * time ~ O(N*M) + O(N*M) + O(V + E), where V - letters.length(), E = min(V^2, N - 1),
+     *      since it is impossible to have more than V^2 edges between V nodes
+     *      also we check N-1 pairs of words. each of them gives at most 1 edge
+     *
+     *      so O(N*M + V + min(V^2, N)) ~ O(N*M + min(V^2, N))
+     *          if V^2 > N:
+     *              since N*M > N => O(N*M + N) ~ O(N*M)
+     *          else (i.e. V^2 < N)
+     *              since V^2 < N < N*M => O(N*M + V^2) ~ O(N*M)
+     *
+     * space ~ O(E + V). since V <= 26 and E < V^2 <= 26^2 = 576 => space ~ O(1)
+     *
+     * 3 attempts:
+     * - not all letters are in keySet => need to traverse through the whole set of letters
+     * - did not consider edge case #1
+     *
+     * BEATS ~ 96%
+     *
+     */
+    public String alienOrder(String[] words) {
+        Map<Character, Set<Character>> adjMap = new HashMap<>();
+        Set<Character> letters = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            for (char c : words[i].toCharArray()) {
+                letters.add(c);
+            }
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+
+            boolean hasSamePrefix = true; //if word1 and word2 have the same prefix which length =  Math.min(word1.length(), word2.length())
+            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
+                if (word1.charAt(j) != word2.charAt(j)) {
+                    adjMap.putIfAbsent(word1.charAt(j), new HashSet<>());
+                    adjMap.get(word1.charAt(j)).add(word2.charAt(j));
+                    hasSamePrefix = false;
+                    break;
+                }
+            }
+
+            //edge case:the words are not sorted lexicographically
+            //apple
+            //app
+            if (hasSamePrefix && word1.length() > word2.length()) return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        Set<Character> visited = new HashSet<>();
+        Set<Character> cycle = new HashSet<>();
+
+        for (Character c : letters) {
+            if (!visited.contains(c)) {
+                if (!topologicalSortDfs(adjMap, c, visited, cycle, sb)) {
+                    return "";
+                }
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+    //    t -> f
+    //    w -> e
+    //    r- > t
+    //    e -> r
+
+    // cycle =
+    // visited = f, t, r e w
+    // sb = ftrew
+
+    private boolean topologicalSortDfs(Map<Character, Set<Character>> adjMap, Character c, Set<Character> visited, Set<Character> cycle, StringBuilder sb) {
+        if (visited.contains(c)) return true;
+
+        if (cycle.contains(c)) return false;
+
+        cycle.add(c);
+
+        for (Character temp : adjMap.getOrDefault(c, new HashSet<>())) {
+            if (!topologicalSortDfs(adjMap, temp, visited, cycle, sb)) {
+                return false;
+            }
+        }
+
+        cycle.remove(c);
+        visited.add(c);
+        sb.append(c);
+
+        return true;
     }
 
     /**
