66f1a1cd3e559e77f3c518b672154ef763ed69b1
krevsky90 <krevskiigrigorii@gmail.com>
2025-02-07 17:13:33 +0300

    Split method: add ValidateIPAddress #468

--- krev/src/solving_techniques/different/ValidateIPAddress.java
+++ krev/src/solving_techniques/different/ValidateIPAddress.java
@@ -1,92 +1,93 @@
 package solving_techniques.different;
 
 /**
- * https://www.tryexponent.com/practice/prepare/validate-ip-address
- * Validate an IP address (IPv4).
- * An address is valid if and only if it is in the form "X.X.X.X",
- * where each X is a number from 0 to 255.
+ * 468. Validate IP Address (medium)
+ * https://leetcode.com/problems/validate-ip-address
  * <p>
- * For example, "12.34.5.6", "0.23.25.0", and "255.255.255.255" are valid IP addresses,
- * while "12.34.56.oops", "1.2.3.4.5", and "123.235.153.425" are invalid IP addresses.
+ * #Company (7.02.2025): 0 - 6 months Meta 4 Microsoft 2 Amazon 2 Turing 2 6 months ago TikTok 6 Apple 4 Oracle 3 ServiceNow 3 Sprinklr 3 Cisco 2 X 2 Deutsche Bank 2
  * <p>
- * Examples:
+ * Given a string queryIP, return "IPv4" if IP is a valid IPv4 address, "IPv6" if IP is a valid IPv6 address or "Neither"
+ * if IP is not a correct IP of any type.
  * <p>
- * ip = '192.168.0.1'
- * output: true
+ * A valid IPv4 address is an IP in the form "x1.x2.x3.x4" where 0 <= xi <= 255 and xi cannot contain leading zeros.
+ * For example, "192.168.1.1" and "192.168.1.0" are valid IPv4 addresses
+ * while "192.168.01.1", "192.168.1.00", and "192.168@1.1" are invalid IPv4 addresses.
  * <p>
- * ip = '0.0.0.0'
- * output: true
+ * A valid IPv6 address is an IP in the form "x1:x2:x3:x4:x5:x6:x7:x8" where:
  * <p>
- * ip = '123.24.59.99'
- * output: true
+ * 1 <= xi.length <= 4
+ * xi is a hexadecimal string which may contain digits, lowercase English letter ('a' to 'f')
+ * and upper-case English letters ('A' to 'F').
+ * Leading zeros are allowed in xi.
+ * For example, "2001:0db8:85a3:0000:0000:8a2e:0370:7334" and "2001:db8:85a3:0:0:8A2E:0370:7334" are valid IPv6 addresses,
+ * while "2001:0db8:85a3::8A2E:037j:7334" and "02001:0db8:85a3:0000:0000:8a2e:0370:7334" are invalid IPv6 addresses.
  * <p>
- * ip = '192.168.123.456'
- * output: false
+ * Example 1:
+ * Input: queryIP = "172.16.254.1"
+ * Output: "IPv4"
+ * Explanation: This is a valid IPv4 address, return "IPv4".
+ * <p>
+ * Example 2:
+ * Input: queryIP = "2001:0db8:85a3:0:0:8A2E:0370:7334"
+ * Output: "IPv6"
+ * Explanation: This is a valid IPv6 address, return "IPv6".
+ * <p>
+ * Example 3:
+ * Input: queryIP = "256.256.256.256"
+ * Output: "Neither"
+ * Explanation: This is neither a IPv4 address nor a IPv6 address.
+ * <p>
+ * Constraints:
+ * queryIP consists only of English letters, digits and the characters '.' and ':'.
  */
 public class ValidateIPAddress {
     /**
-     * KREVSKY SOLUTION
+     * KREVSKY SOLUTION:
+     * time to solve ~ 15 mins
+     * time ~ O(n)
+     * space ~ O(n)
      * <p>
-     * time to solve ~ 30 mins
+     * 3 attempts:
+     * - used split method incorrectly (without limit = -1)
+     * - for IPv4 did not check if 1 <= length <= 3
      * <p>
-     * many attempts
+     * BEATS ~ 94%
      */
-    static boolean validateIP(String ip) {
-        // your code goes here
-        if (ip == null) return false;
+    public String validIPAddress(String queryIP) {
+        if (validateIPv4(queryIP)) {
+            return "IPv4";
+        } else if (validateIPv6(queryIP)) {
+            return "IPv6";
+        } else {
+            return "Neither";
+        }
+    }
 
-        char[] arr = ip.toCharArray();
-        String num = "";
-        int dotCounter = 0;
-        for (int i = 0; i < arr.length + 1; i++) {
-            if (i < arr.length && Character.isDigit(arr[i])) {
-                num += arr[i];
-            } else if (i == arr.length || arr[i] == '.') {
-                if (num.isEmpty()) return false; //..0.0 or 0.0.0.
-                if (num.charAt(0) == '0' && num.length() > 1) return false; //00.0.0.0
-                int numValue = Integer.valueOf(num);
-                if (!(0 <= numValue && numValue <= 255)) return false;
+    private boolean validateIPv4(String ip) {
+        String[] parts = ip.split("\\.", -1);
+        for (String p : parts) {
+            if (!(1 <= p.length() && p.length() <= 3)) return false;
+            if (p.charAt(0) == '0' && p.length() > 1) return false;
+            for (char c : p.toCharArray()) {
+                if (!Character.isDigit(c)) return false;
+            }
+            int num = Integer.valueOf(p);
+            if (num > 255) return false;
+        }
 
-                if (i == arr.length) {
-                    return dotCounter == 3;
-                } else {
-                    dotCounter++;
-                    if (dotCounter > 3) return false;
+        return parts.length == 4;
+    }
 
-                    num = "";
-                }
-            } else if (i < arr.length) {
-                return false;
+    private boolean validateIPv6(String ip) {
+        String[] parts = ip.split("\\:", -1);
+        for (String p : parts) {
+            if (!(1 <= p.length() && p.length() <= 4)) return false;
+            for (char c : p.toCharArray()) {
+                if (!('0' <= c && c <= '9' || 'a' <= c && c <= 'f' || 'A' <= c && c <= 'F')) return false;
             }
         }
 
-        return true;
+        return parts.length == 8;
     }
 
-    /**
-     * SOLUTION #2:
-     * use split method
-     */
-    static boolean validateIP2(String ip) {
-        String[] arr = ip.split("\\.");
-        for (String x : arr) {
-            if (x.isEmpty()) return false;
-            if (x.charAt(0) == '0' && x.length() > 1) return false;
-            int n = new Integer(x);
-            if (n < 0 || n > 255) return false;
-        }
-
-        return arr.length == 4;
-    }
-
-
-
-    public static void main(String[] args) {
-        // debug your code below
-        String ip = "192.168.0.1";
-//        boolean result = validateIP(ip);
-//        System.out.println("Is IP valid? " + result);
-
-        System.out.println(validateIP2(".2..3 "));
-    }
 }
---
+++ krev/src/solving_techniques/different/ValidateIPAddressTryExponent.java
@@ -0,0 +1,100 @@
+package solving_techniques.different;
+
+/**
+ * https://www.tryexponent.com/practice/prepare/validate-ip-address
+ * Validate an IP address (IPv4).
+ * An address is valid if and only if it is in the form "X.X.X.X",
+ * where each X is a number from 0 to 255.
+ * <p>
+ * For example, "12.34.5.6", "0.23.25.0", and "255.255.255.255" are valid IP addresses,
+ * while "12.34.56.oops", "1.2.3.4.5", and "123.235.153.425" are invalid IP addresses.
+ * <p>
+ * Examples:
+ * <p>
+ * ip = '192.168.0.1'
+ * output: true
+ * <p>
+ * ip = '0.0.0.0'
+ * output: true
+ * <p>
+ * ip = '123.24.59.99'
+ * output: true
+ * <p>
+ * ip = '192.168.123.456'
+ * output: false
+ */
+public class ValidateIPAddressTryExponent {
+    /**
+     * KREVSKY SOLUTION
+     * <p>
+     * time to solve ~ 30 mins
+     * <p>
+     * many attempts
+     */
+    static boolean validateIP(String ip) {
+        // your code goes here
+        if (ip == null) return false;
+
+        char[] arr = ip.toCharArray();
+        String num = "";
+        int dotCounter = 0;
+        for (int i = 0; i < arr.length + 1; i++) {
+            if (i < arr.length && Character.isDigit(arr[i])) {
+                num += arr[i];
+            } else if (i == arr.length || arr[i] == '.') {
+                if (num.isEmpty()) return false; //..0.0 or 0.0.0.
+                if (num.charAt(0) == '0' && num.length() > 1) return false; //00.0.0.0
+                int numValue = Integer.valueOf(num);
+                if (!(0 <= numValue && numValue <= 255)) return false;
+
+                if (i == arr.length) {
+                    return dotCounter == 3;
+                } else {
+                    dotCounter++;
+                    if (dotCounter > 3) return false;
+
+                    num = "";
+                }
+            } else if (i < arr.length) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * SOLUTION #2:
+     * use split method
+     */
+    static boolean validateIP2(String ip) {
+        String[] arr = ip.split("\\.", -1);
+        for (String x : arr) {
+            if (!(1 <= x.length() && x.length() <= 3)) return false;
+            if (x.charAt(0) == '0' && x.length() > 1) return false;
+            for (char ch : x.toCharArray()) {
+                if (!Character.isDigit(ch)) return false;
+            }
+            int n = new Integer(x);
+            if (n < 0 || n > 255) return false;
+        }
+
+        return arr.length == 4;
+    }
+
+
+
+    public static void main(String[] args) {
+        // debug your code below
+        String ip = "192.168.0.1";
+//        boolean result = validateIP(ip);
+//        System.out.println("Is IP valid? " + result);
+
+        System.out.println(validateIP2(".2..3 "));
+
+//        "..1.1.1.1...".split("\\.", -1) => ["", "", "1", "1", "1", "", "", ""]
+//        "..1.1.1.1...".split("\\.") => ["", "", "1", "1", "1", "1"]
+//        "...1.1.1.1...".split("\\.+") => ["", "1", "1", "1", "1"]
+
+    }
+}
