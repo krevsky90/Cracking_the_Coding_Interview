c7cdc26de80e367d8efb0eb353e97da479004733
krevsky90 <krevskiigrigorii@gmail.com>
2025-06-20 19:08:58 +0300

    Greedy: add solution to JumpGame #55

--- krev/src/solving_techniques/p17_FibonacciNumbers/JumpGame.java
+++ krev/src/solving_techniques/p17_FibonacciNumbers/JumpGame.java
@@ -5,41 +5,42 @@ import java.util.Arrays;
 /**
  * 55. Jump Game
  * https://leetcode.com/problems/jump-game/
- *
+ * <p>
  * You are given an integer array nums.
  * You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
  * Return true if you can reach the last index, or false otherwise.
- *
+ * <p>
  * Example 1:
  * Input: nums = [2,3,1,1,4]
  * Output: true
  * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
- *
+ * <p>
  * Example 2:
  * Input: nums = [3,2,1,0,4]
  * Output: false
  * Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
- *
+ * <p>
  * Constraints:
  * 1 <= nums.length <= 10^4
  * 0 <= nums[i] <= 10^5
  */
 public class JumpGame {
     public static void main(String[] args) {
-        int[] nums1 = {3,2,1,0,4};
+        int[] nums1 = {3, 2, 1, 0, 4};
         System.out.println(canJump1(nums1));
         System.out.println(canJump2(nums1));
 
     }
+
     /**
      * KREVSKY SOLUTION #1:
      * greedy approach
-     *
+     * <p>
      * idea: the same as src/data_structures/chapter1_arrays_n_strings/amazon_igotanoffer/medium_arrays/Problem2_10_JumpGame # canJump1
      * time to solve ~ 10 mins
      * time ~ O(n)
      * space ~ O(1)
-     *
+     * <p>
      * 2 attempts:
      * - forgot "if (nums.length <= 1) return true"
      */
@@ -55,6 +56,24 @@ public class JumpGame {
             if (maxLength >= nums.length - 1) {
                 return true;
             }
+        }
+
+        return false;
+    }
+
+    /**
+     * KREVSKY SOLUTION #1.2 (20.06.2025)
+     * greedy
+     */
+    public boolean canJumpGeedy(int[] nums) {
+        int max = nums[0];  //not beauty, but works
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i > max) return false;
+            int jump = i + nums[i];
+            if (jump >= n - 1) return true;
+
+            if (max < jump) max = jump;
         }
 
         return false;
